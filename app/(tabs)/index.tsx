0fe44b8ccfeeb66598aea0d169b2944529443a6b
Jaman Nakarmi <zay.nakarmy@gmail.com>
2025-07-14 10:17:25 +0545

    move .env to root

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,14 @@ import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
 import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 export default function Index() {
   const router = useRouter();
@@ -46,6 +53,24 @@ export default function Index() {
               <Text className="text-lg text-white font-bold mt-5 mb-3">
                 Latest Movies
               </Text>
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => (
+                  <Text className="text-white text-sm">{item.title}</Text>
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle = {
+                {
+                  justifyContent: 'flex-start',
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10
+                }
+                }
+                className="mt-2 pb-32"
+                scrollEnabled ={false}
+              ></FlatList>
             </>
           </View>
         )}

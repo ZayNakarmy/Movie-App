42fbc06e1ea0bd00e0fba8c924547a3fe49404fd
Jaman Nakarmi <zay.nakarmy@gmail.com>
2025-07-14 16:07:50 +0545

    masked view fix

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,5 +1,5 @@
 import { images } from "@/constants/images";
-import { MaskedViewComponent } from "@react-native-masked-view/masked-view";
+import MaskedView from "@react-native-masked-view/masked-view";
 import { Link } from "expo-router";
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
@@ -17,7 +17,7 @@ const TrendingCard = ({
           resizeMode="cover"
         />
         <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
-          <MaskedViewComponent
+          <MaskedView
             maskElement={
               <Text className="font-bold text-white text-6xl">{index + 1}</Text>
             }
@@ -27,8 +27,14 @@ const TrendingCard = ({
               className="size-14"
               resizeMode="cover"
             />
-          </MaskedViewComponent>
+          </MaskedView>
         </View>
+        <Text
+          className="text-sm font-bold mt-2 text-light-200"
+          numberOfLines={2}
+        >
+          {title}
+        </Text>
       </TouchableOpacity>
     </Link>
   );
